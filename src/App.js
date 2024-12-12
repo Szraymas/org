1581afd610781ea428f70240e06c1087dca769a6
Felipe Suárez <suarez.cdfp12fsm@gmail.com>
2024-12-12 16:10:46 -0500

    Trabajo completado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,147 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { v4 as uuid } from "uuid"
 import './App.css';
+import Header from './componentes/Header/Header';
+import Formulario from './componentes/Formulario/Formulario';
+import MiOrg from './componentes/MiOrg';
+import Equipo from './componentes/Equipo';
+import Footer from './componentes/Footer';
 
 function App() {
+  const [mostrarFormulario, actualizarMostrar] = useState(false)
+  const [colaboradores, actualizarColaboradores] = useState([{
+    id: uuid(),
+    equipo: "Front End",
+    foto: "https://avatars.githubusercontent.com/u/129417878?v=4",
+    nombre: "Felipe Suárez Másmela",
+    puesto: "Estudiante",
+    fav: true
+  }])
+
+  const [equipos, actualizarEquipos] = useState([
+
+    {
+      id: uuid(),
+      titulo: "Programación",
+      colorPrimario: "#57c278",
+      colorSecundario: "#D9F7E9"
+    },
+    {
+      id: uuid(),
+      titulo: "Front End",
+      colorPrimario: "#82CffA",
+      colorSecundario: "#E8F8ff"
+    },
+    {
+      id: uuid(),
+      titulo: "Data Science",
+      colorPrimario: "#a6d157",
+      colorSecundario: "#f0f8e2"
+    },
+    {
+      id: uuid(),
+      titulo: "Devops",
+      colorPrimario: "#e06b69",
+      colorSecundario: "#fde7e8"
+    },
+    {
+      id: uuid(),
+      titulo: "UX y Diseño",
+      colorPrimario: "#db6ebf",
+      colorSecundario: "#fae9f5"
+    },
+    {
+      id: uuid(),
+      titulo: "Móvil",
+      colorPrimario: "#ffba05",
+      colorSecundario: "#fff5d9"
+    },
+    {
+      id: uuid(),
+      titulo: "Innovación y Gestión",
+      colorPrimario: "#ff8a29",
+      colorSecundario: "#ffeedf"
+    }
+
+    
+ 
+  ])
+
+  // Ternario -> condicion ? seMuestra : noSeMuestra
+  // condicion && seMuestra
+
+  const cambiarMostrar = () => {
+    actualizarMostrar(!mostrarFormulario)
+  }
+
+  //Registrar colaborador
+
+  const registrarColaborador = (colaborador) => {
+    //Spread operator
+    actualizarColaboradores([...colaboradores, colaborador])
+  }
+
+  // Eliminar Colaborador
+  const eliminarColaborador = (id) => {
+    const nuevosColaboradores = colaboradores.filter((colaborador) => colaborador.id !== id)
+    actualizarColaboradores(nuevosColaboradores)
+  }
+
+  //Actualizar el color de equipo
+  const actualizarColor = (color, id) => {
+    const equiposActualizados = equipos.map((equipo) => {
+      if (equipo.id === id){
+        equipo.colorPrimario = color
+      }
+
+      return equipo
+    })
+
+    actualizarEquipos(equiposActualizados)
+  }
+
+  //Crear Equipo
+  const crearEquipo = (nuevoEquipo) => {
+    actualizarEquipos([...equipos, { ...nuevoEquipo, id: uuid() }])
+  }
+
+  const like = (id) => {
+    const colaboradoresActualiazados = colaboradores.map((colaborador) => {
+      if(colaborador.id === id){
+        colaborador.fav = !colaborador.fav
+      }
+      return colaborador
+    })
+
+    actualizarColaboradores(colaboradoresActualiazados)
+  }
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Header />
+      { mostrarFormulario ? <Formulario 
+        equipos={equipos.map((equipo) => equipo.titulo)} 
+        registrarColaborador = {registrarColaborador}
+        crearEquipo={crearEquipo} /> : <></> 
+        }
+      
+      <MiOrg cambiarMostrar = {cambiarMostrar}/>
+
+      {
+        equipos.map( (equipo) => <Equipo 
+          datos={equipo} 
+          key={equipo.titulo} 
+          colaboradores={colaboradores.filter( colaborador => colaborador.equipo === equipo.titulo )} 
+          eliminarColaborador={eliminarColaborador}
+          actualizarColor={actualizarColor}
+          like={like}
+        />
+      )
+      }
+
+      <Footer />
+
     </div>
   );
 }
